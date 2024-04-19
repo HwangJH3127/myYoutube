ffe2928d30075a3c5a0cac26651c50462107da34
jonghyeon <whdgusdldl22@naver.com>
2024-04-19 11:43:36 +0900

    최종 수정

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Logo = () => {
+const Logo = ({ toggleMenu }) => {
   return (
     <h1 className="header__logo">
       <Link to="/">
-        <em aria-hidden="true"></em>
+        <em aria-hidden="true" onClick={toggleMenu}></em>
         <span>
           hwang's <br />
           YOUTUBE
--- src/components/section/Header.jsx
+++ src/components/section/Header.jsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Logo from "../header/Logo";
 import Sns from "../header/Sns";
 import Menu from "../header/Menu";
 
 const Header = () => {
+  const [isMenuActive, setIsMenuActive] = useState(false);
+  const toogleMenu = () => {
+    setIsMenuActive(!isMenuActive);
+  };
   return (
-    <header id="header" role="banner">
-      <Logo />
+    <header id="header" role="banner" className={isMenuActive ? "active" : ""}>
+      <Logo toggleMenu={toogleMenu} />
       <Menu />
       <Sns />
     </header>
