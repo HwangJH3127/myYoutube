28aa261b7354dbd9a9b72826f3f5de7afbd5ca0d
jonghyeon <whdgusdldl22@naver.com>
2024-04-19 10:19:03 +0900

    Rapid API 및 더보기

--- src/components/section/Search.jsx
+++ src/components/section/Search.jsx
@@ -4,14 +4,11 @@ import { useNavigate } from "react-router-dom";
 const Search = () => {
   const [searchKeyword, setSearchKeyword] = useState("");
   const navigate = useNavigate();
-
   const handleSearch = () => {
     if (searchKeyword) {
       navigate(`/search/${searchKeyword}`);
-      setSearchKeyword("");
     }
   };
-
   return (
     <div id="search">
       <div className="search__inner">
@@ -25,7 +22,9 @@ const Search = () => {
           autoComplete="off"
           className="search__input"
           placeholder="검색어를 입력해주세요."
-          onChange={(e) => setSearchKeyword(e.target.value)}
+          onChange={(e) => {
+            setSearchKeyword(e.target.value);
+          }}
           onKeyDown={(e) => {
             if (e.key === "Enter") {
               handleSearch();
